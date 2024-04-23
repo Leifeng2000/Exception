6de58af5b50e4a47367d18c31d2ce3a3340b55da
Leifeng2000 <leifeng7112@gmail.com>
2024-04-23 20:37:20 +0700

    Upd

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] myArray = {0,1,2,3,4,5,6,7,8,9};
+        try
+        {
+            Console.WriteLine(myArray[11]);
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine($"{ex.Message}");
+        }
+        finally
+        {
+            // cac lenh duoc thuc thi
+        }
+        //
+        int a = 10;
+        int b = 0;
+        try
+        {
+            try
+        {
+            int c = a / b;
+        }
+        catch (System.Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        }
+        catch (DivideByZeroException ex)
+        {
+            
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
